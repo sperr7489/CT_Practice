546951c33dc38c1b24f8bc1a1f1ac9cd1e4d9e0f
sperr <sperr@ajou.ac.kr>
2022-03-19 18:14:21 +0900

    팰린드론

--- 파알인/Exam/01.py
+++ 파알인/Exam/01.py
@@ -23,3 +23,18 @@ else:
     print("이건 아니지")
 
 # 책에서 제시하는 코드
+
+
+def isPalindrome(self, s: str) -> bool:
+    strs = []
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    # 팰린드롬 여부 판별
+    while len(strs) > 1:
+        if strs.pop(0) != strs.pop():
+            return False
+    return True
+    # pop 함수를 통해서 리스트의 반환값을 맨 앞의 값과 맨 뒤의 값을
+    # 비교해 나가면서 다른 것이 있다면 False를 반환하는 방식으로 진행한다.
