3e7613bd236e19e0c6b8a86cceaac5c4580f386b
sperr <sperr@ajou.ac.kr>
2022-03-21 19:21:48 +0900

    leetcode most common word

---
+++ 파알인/Exam/ch04.py
@@ -0,0 +1,32 @@
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+
+
+array = paragraph.split()
+
+compare_list = []
+
+for char in array:
+    # char = ''.join(filter(str.isalnum, char))
+    char = "".join(filter(lambda x: x.isalnum(), char))
+
+    compare_list.append(char.lower())  # 전부 lower로 바꾸어준다.
+
+print(compare_list)
+
+max_str = ""
+
+flag = 0
+
+for char in compare_list:
+    idx = 0
+    if char in banned:
+        continue
+    for tmp_char in compare_list:
+        if char == tmp_char:
+            idx += 1
+    if flag < idx:
+        flag = idx
+        max_str = char
+
+print(max_str)
