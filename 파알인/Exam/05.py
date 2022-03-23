14bcc5c836466763cc425444463c85fabded23e3
sperr <sperr@ajou.ac.kr>
2022-03-24 00:47:56 +0900

    anagram의 defaultdict 사용성

--- 파알인/Exam/05.py
+++ 파알인/Exam/05.py
@@ -2,7 +2,8 @@
 # from tracemalloc import start
 
 
-# strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+import collections
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 
 # sort_strs = []
 
@@ -37,13 +38,21 @@
 # print(result)
 
 
-import collections
-from typing import Collection
+anagrams = collections.defaultdict(list)
 
+for word in strs:
+    anagrams[''.join(sorted(word))].append(word)
+    # ''.join(sorted(word))의 키를 새롭게 생성.=>
+    # 키에 해당하는 값을 리스트 형태로 append시켜준다.
+    # defaultdict(list)를 했기 때문에 다음과 같은 처리가 가능해진것이다.
+    # 이를 해주지 않으면 존재하지않는 key에 접근했다는 keyError가 발생하게 된다.
+print(list(anagrams.values()))
+print(anagrams)
 
 s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
 d = collections.defaultdict(list)
+# d = dict()
 for k, v in s:
     print(v)
-    d[k].append(v)
-print((d.items()))
+    d[k] = v
+print(d)
