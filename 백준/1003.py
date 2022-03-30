cab66604d4085e11a0fbb0bf8e3de94c7318182b
sperr7489 <sperr@ajou.ac.kr>
2022-03-31 00:59:45 +0900

    백준 1003 complete

--- 백준/1003.py
+++ 백준/1003.py
@@ -1,43 +1,42 @@
-from ast import List
-import collections
-from itertools import count
+# fibo(n)의 0의 갯수 = fibo(n-2)의 0의 갯수 + fibo(n-1)의 0의 갯수
+# fibo(n)의 1의 갯수 = fibo(n-2)의 1의 갯수 + fibo(n-1)의 1의 갯수
 
+# 결과값을 하나씩 저장하여 이에 해당하는 딕셔너리의 key값을 찾아가도록 하는 방식
 
-test_case = 4
+t =int(input())
 
-
-# def fibonacci(n: int) -> int:
-#     if n == 0:
-#         print(0)
-#         return 0
-#     elif n == 1:
-#         print(1)
-#         return 1
-#     else:
-#         return fibonacci(n-1) + fibonacci(n-2)
-
-
-def fibonacci_array(n: int) -> list[int]:
-    if n == 0:
-        # print(0)
-        return [0]
-    elif n == 1:
-        # print(1)
-        return [1]
+result ={ 0: [1,0],1:[0,1],2:[1,1]}
+# 키가 있는 지 우선적으로 검사한다. 
+def keyCheck(n :int):
+    if n in result:
+        return True
     else:
-        return fibonacci_array(n-1) + fibonacci_array(n-2)
+        return False
 
-
-# print(fibonacci_array(22))
-
-# fibonacci_array의 리턴이 배열로 제시된다. 얘를들어 [1,]
-
-def fibonacci_print(n: int):
-    counts = collections.defaultdict(int)
-    for i in fibonacci_array(n):
-        counts[i] += 1
-    print(counts[0], end=" ")
-    print(counts[1])
-
-
-def print_result(t: int, n: list[int]):
+def fibo_count(n: int)->bool:
+    if keyCheck(n):
+        return True
+    if keyCheck(n-2) and keyCheck(n-1):
+        # n의 result 값이 존재할 경우 
+        result[n]=result[n-2][0]+result[n-1][0],result[n-2][1]+result[n-1][1]
+        return True
+    if not keyCheck(n-2):
+        # n-2의 키가 없을 경우?
+        fibo_count(n-2)
+    if not keyCheck(n-1):  
+        fibo_count(n-1)
+    fibo_count(n)
+for i in range(t):
+    n = int(input())
+    fibo_count(n) #테스트케이스를 하나씩 받음
+    print(result[n][0],result[n][1])
+            
+            
+# 내가 생각한 모범 답안.
+for i in range(int(input())):
+    a,b=1,0
+    for i in range(int(input())): a,b=b,a+b
+    print(a,b)
+    # 나의 로직을 굉장히 단순하게 표현했다. 
+    # 피보나치의 결과값의 1과 0의 갯수가 갖는 규칙을 이렇게 단순하게 표현하다니
+    # 굉장히 충격적이다. 
