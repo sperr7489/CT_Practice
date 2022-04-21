0589d46d99c9db5fa9087d51d198c202910e8c99
sperr <sperr@ajou.ac.kr>
2022-04-21 17:57:27 +0900

    runtimeerror를 해결해야한다. Index 처리 문제인듯 하다.

--- 백준/1012.py
+++ 백준/1012.py
@@ -1,64 +1,50 @@
 
-from typing import Collection, List
+# 입력받은 좌표에 따라서 먼저 리스트를 만든다.
 
 
-# t = int(input()) # 테스트 케이스의 갯수
-# m = int(input()) # 가로길이
-# n = int(input()) # 세로길이
-bug = {}
+from typing import List
 
 
-def cu_neighbor(first, second):  # 인접한 것을 판단하는 함수.
-    # List(int)로 받는 이유는 [x좌표, y좌표]를 의미
-    # first가 비교되는 들어온 값, second가 비교할 대상.
-
-    if first[0] == second[0]:  # x좌표가 같을 때
-        if abs(first[1]-second[1]) == 1:
-            # y좌표의 차의 절댓값이 1일 때 인접한다고 할 수 있다.
-            return True
-        else:
-            return False
-    if first[1] == second[1]:
-        if abs(first[0]-second[0]) == 1:
-            # x좌표의 차의 절댓값이 1일 때 인접한다고 할 수 있다.
-            return True
-        else:
-            return False
+t = input()  # 테스트케이스의 갯수
 
 
-for i in range(int(input())):  # 배추의 갯수 : k
-    # 임의의 갯수의 정수를 한 줄에 입력받아 리스트에 저장.
-    first = list(map(int, sys.stdin.readline().split()))
-    if i == 0:
-        # 첫번째 들어온것.
-        bug[1] = [first]
-        second = first
-        continue
-    else:
-        # 두번째부터
-        if cu_neighbor(first, second):
-            # 만약 인접하다면 해당 벌레에 리스트형태로 추가해야겠지?
-            for k in bug:
-                if second in bug[k]:
-                    # bug에 이미 해당 second가 존재한다면?
-                    bug[k].append(first)
-                    break
-        else:
-            # 인접하지 않다면?
-            length = len(bug.keys())
-            bug[length] = [first]
+def num_is_bugs() -> int:
+    # 가로길이, 세로길이, 배추의 갯수를 입력받는다
 
-    second = first
+    col, row, vegi_num = map(int, input().split())
+
+    farm = [[0]*col for _ in range(row)]  # 리스트 초기화 row * col의 배열
+    # 가로길이는 사실상 col의 갯수, 세로 길이는 row의 갯수가 된다.
+    for i in range(vegi_num):
+        x, y = map(int, input().split())  # 인풋 맏은 것을 int형태로 입력받는다.
+        # x와 y좌표를 입력받는다.
+        farm[x][y] = 1
+        # 여기까지가 농장의 배추가 심어진 위치를 알려준것이라고 할 수 있다.
+
+    def dfs(i, j):
+        # 더 이상 땅이 아닌 경우 종료
+        if i < 0 or i >= len(farm) or \
+            j < 0 or j >= len(farm[0]) or \
+                farm[i][j] != 1:
+            return
+
+        farm[i][j] = 0  # def함수는 중첩함수이기 때문에 부모 함수의 변수를 맘대로
+        # 쓸 수 있기 때문에 farm 변수를 맘대로 가져올 수 있다.
+        dfs(i+1, j)
+        dfs(i-1, j)
+        dfs(i, j+1)
+        dfs(i, j-1)
+
+    count = 0
+    for i in range(len(farm)):
+        for j in range(len(farm[0])):
+            if farm[i][j] == 1:
+                dfs(i, j)
+                # 모든 육지 탐색 후 카운트 1 증가
+                count += 1
+
+    print(count)
 
 
-# bug[1]=[[1,2]]
-# bug[2]=[3,4]
-# print(bug)
-# bug[1].append([6,7])
-# for i in bug:
-#     print(bug[i])
-# print(bug)
-a = [1, 2]
-print(a)
-a = [3, 5]
-print(a)
+for t in range(int(t)):
+    num_is_bugs()
