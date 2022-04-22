e3cbdf4188433ad2953893e636fc98fe8bf9ef7d
sperr <sperr@ajou.ac.kr>
2022-04-22 20:56:36 +0900

    백준 1012

--- 백준/1012.py
+++ 백준/1012.py
@@ -2,10 +2,14 @@
 # 입력받은 좌표에 따라서 먼저 리스트를 만든다.
 
 
+import sys
 from typing import List
 
 
 t = input()  # 테스트케이스의 갯수
+
+
+sys.setrecursionlimit(10000)
 
 
 def num_is_bugs() -> int:
@@ -14,7 +18,6 @@ def num_is_bugs() -> int:
     col, row, vegi_num = map(int, input().split())
 
     farm = [[0]*col for _ in range(row)]  # 리스트 초기화 row * col의 배열
-    print(farm)
     # 가로길이는 사실상 col의 갯수, 세로 길이는 row의 갯수가 된다.
     for i in range(vegi_num):
         x, y = map(int, input().split())  # 인풋 맏은 것을 int형태로 입력받는다.
@@ -26,15 +29,15 @@ def num_is_bugs() -> int:
         # 더 이상 땅이 아닌 경우 종료
         if i < 0 or i >= len(farm) or \
             j < 0 or j >= len(farm[0]) or \
-                farm[j][i] != 1:
+                farm[i][j] != 1:
             return
 
         farm[i][j] = 0  # def함수는 중첩함수이기 때문에 부모 함수의 변수를 맘대로
         # 쓸 수 있기 때문에 farm 변수를 맘대로 가져올 수 있다.
-        dfs(j+1, i)
-        dfs(j-1, i)
-        dfs(j, i+1)
-        dfs(j, i-1)
+        dfs(i+1, j)
+        dfs(i-1, j)
+        dfs(i, j+1)
+        dfs(i, j-1)
 
     count = 0
     for i in range(len(farm)):
@@ -44,8 +47,11 @@ def num_is_bugs() -> int:
                 # 모든 육지 탐색 후 카운트 1 증가
                 count += 1
 
-    print(count)
+    return count
 
 
-for t in range(int(t)):
-    num_is_bugs()
+answer = []
+for i in range(int(t)):
+    answer.append(num_is_bugs())
+for i in range(int(t)):
+    print(answer[i])
