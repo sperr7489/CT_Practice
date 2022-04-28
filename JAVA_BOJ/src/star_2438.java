b42afcdc273a1601d37be9f824ca45ce0b1c608e
안병준 <neostgeart@gmail.com>
2022-04-28 12:58:11 +0900

    백준 2438

---
+++ JAVA_BOJ/src/star_2438.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class star_2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt(); // 몇번째 줄까지 할 것인지.
+        for(int i =0; i<num; i++){
+            for (int j=num-i-1;j<num; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
