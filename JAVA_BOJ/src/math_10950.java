6078a7846f62af6ffc384ec6838a0d8fb6cf80d3
sperr7489 <sperr@ajou.ac.kr>
2022-04-30 00:21:36 +0900

    EOF가 될때까지 입력받는 방법과 StringTokenizer를 사용하여 문자열을 자르는 법.

---
+++ JAVA_BOJ/src/math_10950.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class math_10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int t = Integer.parseInt(str);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        String input;
+        while ( (input = br.readLine()) != null ){
+                // 만약 위에서처럼 null이 되었다면 입력값이 입력되지 않았다는 뜻
+            StringTokenizer st = new StringTokenizer(input);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr.add(sumString(a,b));
+        }
+        for(int a :arr){
+            System.out.println(a);
+        }
+
+    }
+    public static int sumString(int a,int b){
+        return a+b;
+    }
+}
