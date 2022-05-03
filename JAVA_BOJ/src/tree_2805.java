a7dc69f2c4fa273e7e5f230b5130df54f64351ac
qro <sperr@ajou.ac.kr>
2022-05-03 17:25:04 +0900

    2085 나무잘라내기 실버2 문제 해결! 이분 탐색의 끝장나는 공부!

--- JAVA_BOJ/src/tree_2805.java
+++ JAVA_BOJ/src/tree_2805.java
@@ -15,75 +15,71 @@ public class tree_2805 {
         int num = Integer.parseInt(str[0]);
         long wanted = Long.parseLong(str[1]);
         String input[] = br.readLine().split(" ");
-        Long[] arr =new Long[num];
+        long[] arr =new long[num];
 
         for(int i =0; i< num;i++){
             arr[i]= Long.parseLong(input[i]);
         }
-        Arrays.sort(arr);
-        System.out.println(checkRangeOfHeight(arr,wanted));
+
+        long max = getMax(arr);
+        long result = binarySearch(max, arr, wanted);
+        System.out.println(result);
+
+
     }
-    public static long checkRangeOfHeight(Long[] height,long wanted){
+//    public static long checkRangeOfHeight(Long[] height,long wanted){
+//    }
 
-        int start = 0;
-        int end = height.length-1;
-        int mid  = (start+end)/2;
+    public static long binarySearch(long max,long[] height,long wanted){ // 정렬된 배열을 입력받는다.
+        //각각의 index를 첫,끝지점으로 지정.
+        long start = 0;
+        long end = max;
+        long mid = (start+end)/2;
 
+        boolean unChecked = true;
 
-
-        boolean unChecked= true;
-
-        if(height.length ==1)
-        {
-            return height[0]-wanted;
-        }
-
-        if(height[end]-height[end-1]>wanted){
-            return height[end]-wanted;
-        }
         while(unChecked){
-            if(sumOfTreeCut(height,height[mid]) == wanted) {
-                return height[mid];
-            } else if (sumOfTreeCut(height,height[mid]) < wanted) {
-                // 원하는 값보다 계산의 결과값이 더 작을경우.
-                if(start ==mid){
-                    long resultSum = 0;
-                    for(int i= start; i<height.length;i++){
-                        resultSum +=height[i];
-                    }
-                    long result = (resultSum-wanted)/(height.length-start);
-                    return result;
-                }
-                end = mid -1;
-                mid = (start+end)/2;
-            }else{
-                //원하는 값보다 계산의 결과값이 더 클 경우.
+            long sum = sumOfTreeCut(height,mid);
+
+            if(wanted == sum ){
+                break;
+            } else if (wanted < sum) {
+                //원하는 값보다 계산된 값이 더 크다면 톱날의 길이가 더 높아져야 함.
                 start = mid+1;
                 mid = (start+end)/2;
+            }else{
+                //원하는 값이 계산된 값보다 더 크다면 톱날의 길이가 더 짧아져야함(많이 짤라내야 하므로)
+                end = mid -1;
+                mid = (start+end)/2;
             }
-            if(start == end) {
-                unChecked = false; //반복문을 끝낸다.
+            if(start > end)  {
+                //start가 end보다 커졌다는 것은 이전에 mid로 잘랐을 때 자르는 양이 wanted보다 작았을 때 end = mid-1이 이루어졌을 때 !!
+                mid = end;
+                break;
             }
         }
-        long resultSum =0;
-        for(int i = mid+1; i<height.length;i++){
-            resultSum +=height[i];
-        }
-        long result = (resultSum-wanted)/(height.length-mid-1);
-
-
-        return result;
-
+        return mid;
+//        height[start]와 height[end]를 따져봐야할텐데 말이야. mid 를 기준으로 바뀔 수 있다.
     }
-    public static long sumOfTreeCut(Long[] height, long cutting){
+    public static long sumOfTreeCut(long[] height, long cutting){
         long sum=0;
         for (long a :
                 height) {
-            if(a>=cutting){
+            if(a>cutting){
                 sum+=(a-cutting);
             }
         }
 
         return sum;
+    }
+    public static long getMax(long[] arr){
+        long max = 0;
+
+        for (long a :
+                arr) {
+            max = Math.max(max,a);
+        }
+        return max;
+
     }
 }
