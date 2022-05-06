1c5d2de3e6178ab917001cf35f6fbaaf96d4ac2d
sperr <sperr@ajou.ac.kr>
2022-05-06 18:34:20 +0900

    이분탐색으로 mid의 일반적인 위치를 찾고 최솟값은 반복문으로 돌아가면서 알아보았지만 여전히 시간탐색 문제를 해결할 수가 없다.

--- JAVA_BOJ/src/math_1072.java
+++ JAVA_BOJ/src/math_1072.java
@@ -7,28 +7,30 @@ public class math_1072 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str[] = br.readLine().split(" ");
-        long gameCount = Integer.parseInt(str[0]);
-        long winCount =  Integer.parseInt(str[1]);
-        long winPercent = winCount/gameCount*100;
+        double gameCount = Integer.parseInt(str[0]);
+        double winCount =  Integer.parseInt(str[1]);
+        long winPercent = (long)(winCount/gameCount*100);
 
+        long tempResult = 0;
 
-        if(gameCount == winCount){
+        if(gameCount == winCount || winPercent >=99){
             System.out.println("-1");
         }
         else{
-            System.out.println(calNewWinVal(gameCount,winCount,winPercent));
+            tempResult = calNewWinVal(gameCount,winCount,winPercent);// tempResult에서 하나씩 줄여보면서 확인을 해야한다.
+            System.out.println(minResult(gameCount,winCount,winPercent,tempResult));
         }
 
     }
 
-    public static long calNewWinVal(long gameCount, long winCount, long winPercent  ){
+    public static long calNewWinVal(double gameCount, double winCount, double winPercent  ){
 
-        long start = 1;
+        long start = 0;
         long end = max;
         int x  =0; // 게임을 몇번 추가적으로 더해야하는지 판단하기 위한 변수.
         boolean flag = true;
 
-        long newWinPercent;
+        long newWinPercent = 0;
 
         long type ;
         long mid = 0  ;
@@ -37,21 +39,38 @@ public class math_1072 {
             mid = (start+end)/2;
             //x를 하나씩 늘려나간다.
 //            newWinPercent = (winCount+x)/(gameCount+x)*100;
-                newWinPercent = (winCount+mid) /(gameCount+mid)*100;
-                type = newWinPercent-winPercent;
+            newWinPercent = (long)((winCount+mid)/( gameCount+mid) *100);
 
+            type = (long)(newWinPercent-winPercent); // type는 반드시 정수라는 것!
+//            System.out.println(mid);
+            // TODO: 2022/05/05 while 문 안에서 자꾸 무한 루프가 돌고 있는 모습을 확인할 수 있다. 이부분을 해결하는 것이 급선무
                 if(type>1){
                     //새로운 통계값과의 차이가 1보다 크다면 더 줄일 수 있다는 것.
                     end = mid-1;
-                } else if (type ==0) {
+                } else if (type ==1) {
                     //차이가 없으면 start를 더 키워야겠지
-                    start = mid+1;
-                } else if (type==1) {
+                    flag=false; //이부분에서 정답이 된다.
+                } else {
                     //정답!!
-                    flag= false;
+                    start = mid+1;
                 }
         }
         return mid;
     }
+    public static long  minResult(double gameCount, double winCount, double winPercent,long tempResult){
 
+        boolean flag= true;
+        double newWinPercent;
+        long type = 0;
+        while(flag){
+            tempResult--;
+            newWinPercent = (winCount+tempResult) /(gameCount+tempResult)*100;
+            type = (long)(newWinPercent - winPercent);
+            if(type == 0){
+                flag = false;
+                ++tempResult;
+            }
+        }
+        return tempResult;
+    }
 }
