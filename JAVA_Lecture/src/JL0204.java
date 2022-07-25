2913937b373b57b9d3fbd0a5a2785ab066c16f22
sperr7489 <sperr@ajou.ac.kr>
2022-07-25 17:57:03 +0900

    java 강의 0204 해결

---
+++ JAVA_Lecture/src/JL0204.java
@@ -0,0 +1,36 @@
+package JAVA_Lecture.src;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class JL0204 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int testcase = in.nextInt();
+        List<Integer> arr = new ArrayList<>();
+        solution(arr,testcase,0);
+
+        System.out.println(String.join(" ",arr.toString().split(",")));
+
+//        for (Integer a :
+//                arr ) {
+//            System.out.print(a+" ");
+//        }
+    }
+    public static void solution(List<Integer> arr, int t,int index ){
+        if(t-index ==1){
+            arr.add(arr.get(index-2)+arr.get(index-1));
+            return;
+        }
+        if(index<2)
+        {
+            arr.add(1);
+            solution(arr,t,++index);
+            return;
+        }
+        arr.add(arr.get(index-2)+arr.get(index-1));
+        solution(arr,t,++index);
+    }
+
+}
